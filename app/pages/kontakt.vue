59abc1aa7b707e550a3d991de5987d9259ffd127
Jacobb <146146421+engineerie@users.noreply.github.com>
2025-09-19 17:25:25 +0100

    applied dynamic to UIcon component itself

--- app/pages/kontakt.vue
+++ app/pages/kontakt.vue
@@ -32,15 +32,15 @@ useSeoMeta({
         <template #description>
           <div class="space-y-1 text-left w-full">
             <div class="flex items-center gap-2">
-              <UIcon :name="page.phone.icon" class="text-daniel-500" />
+              <UIcon name="i-heroicons-phone-20-solid" class="text-daniel-500" />
               <a :href="'tel:' + page.phone.number" class="hover:underline">{{ page.phone.number }}</a>
             </div>
             <div class="flex items-center gap-2">
-              <UIcon :name="page.email.icon" class="text-daniel-500" />
+              <UIcon dynamic :name="page.email.icon" class="text-daniel-500" />
               <a :href="'mailto:' + page.email.address" class="hover:underline">{{ page.email.address }}</a>
             </div>
             <div class="flex gap-2">
-              <UIcon :name="page.location.icon" class="text-daniel-500 mt-0.5" />
+              <UIcon dynamic :name="page.location.icon" class="text-daniel-500 mt-0.5" />
               <div>
                 <div>{{ page.location.street }}</div>
                 <div>{{ page.location.postCode }}</div>
