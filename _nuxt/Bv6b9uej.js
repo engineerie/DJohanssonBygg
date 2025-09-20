9249b4a00d51d0b523f7534d087b094905562164
engineerie <146146421+engineerie@users.noreply.github.com>
2025-09-20 09:24:06 +0000

    Deploying to gh-pages from @ engineerie/DJohanssonBygg@70fae452dc80b631ad555dc194a5ff85936e88a7 🚀

---
+++ _nuxt/Bv6b9uej.js
@@ -0,0 +1,1 @@
+import{r as n}from"./CGDRqLL5.js";import a from"./BeeJCVuO.js";import{y as s,G as t,d as e,w as i,S as p}from"./DRTcvyly.js";import"./iik6CYzq.js";const d={__name:"ProseWarning",setup(c){const o=s();return(r,f)=>(e(),t(a,{color:"warning",icon:p(o).ui.icons.warning},{default:i(()=>[n(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
