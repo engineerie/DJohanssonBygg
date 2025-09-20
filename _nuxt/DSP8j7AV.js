d49db9a9eb42fe063b48915569bf66062f63c158
engineerie <146146421+engineerie@users.noreply.github.com>
2025-09-20 12:46:30 +0000

    Deploying to gh-pages from @ engineerie/DJohanssonBygg@252d947a65ba483c018a76233fccb023d95c6a2d 🚀

--- _nuxt/DSP8j7AV.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./Bhk4v0yz.js";import t from"./jrnwFX_U.js";import{y as e,G as a,d as p,w as c,S as n}from"./CwAFs6-9.js";import"./iik6CYzq.js";const d={__name:"ProseTip",setup(i){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
