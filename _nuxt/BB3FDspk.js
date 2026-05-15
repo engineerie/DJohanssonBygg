b0b8667d1c0ba76e9bb2d1805013fa9b9f4440bc
engineerie <146146421+engineerie@users.noreply.github.com>
2026-05-15 14:43:36 +0000

    Deploying to gh-pages from @ engineerie/DJohanssonBygg@3fc9c1560193653d9043e5b0b7ef6c3f91d23900 🚀

--- _nuxt/BB3FDspk.js
+++
@@ -1,1 +0,0 @@
-import{r as a}from"./DcmsyPNj.js";import e from"./BwjZvlMg.js";import{x as t,a1 as n,d as s,w as i,ae as p}from"./D5H0XVUs.js";import"./iik6CYzq.js";const d={__name:"ProseNote",setup(c){const o=t();return(r,f)=>(s(),n(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[a(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
